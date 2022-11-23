ab049e2cff2345c1036552e3e9eb1084533d12da
Tealiosv2 <97336208+Tealiosv2@users.noreply.github.com>
2022-11-23 01:42:34 -0800

    music commands setup in index.js
    setting up discord bot for slash commands for music bot

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import { config } from 'dotenv'; // import config from .env
 import { Client, CommandInteractionOptionResolver, GatewayIntentBits, Routes } from 'discord.js';
 import { REST } from 'discord.js'; // REST API used for grabbing data from site
+const {REST} = require("@discordjs/rest")
+const{ Routes } = require("@discord-api-type/v9")
+const fs = require("fs")
+const { Player } = require("discord-player")
+
+const LOAD_SLASH = process.argv[2] == "load"
+
+
 
 config(); // loads data from .env
 
@@ -14,11 +22,48 @@ const GUILD_ID = process.env.guildId;
 // Intents allows ability to choose which events to import?
 const client = new Client({ 
     intents: [
+        "GUILDS",
+        "GUILD_VOICE_STATES",
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMessages,
         GatewayIntentBits.MessageContent,
     ],
 });
+
+client.slashcommands = new DiscordAPIError.Collection()
+client.player = new Player(client, {
+    ytldOptions: {
+        quality: "highestaudio",
+        highWaterMark: 1 << 25
+    }
+})
+
+let commands = []
+
+const slashFiles = fs.readdirSync("./slash").filter(file => file.endsWith(".js"))
+
+for(const file of slashFiles){
+    const slashcmd = require('./slash/${file}')
+    client.slashcommands.set(slashcmd.data.name, slashcmd)
+
+    if (LOAD_SLASH) commands.push(slashcmd.data.toJSON())
+}
+
+if (LOAD_SLASH){
+    const rest = new REST({version: "9"}).setToken(TOKEN)
+    console.log("Deploying slash commands")
+    rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), {body: commands})
+    .then(() => {
+        console.log("Successfully loaded")
+        process.exit(0)
+    })
+    .catch((err) => {
+        if(err){
+            console.log(err)
+            process.exit(1)
+        }
+    })
+}
 
 // retrieve token
 const rest = new REST({ version: '10' }).setToken(TOKEN);
